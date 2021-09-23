a247e7ff30676401af54b15c721ed08f657dffbf
Serhii <malgothsm@gmail.com>
2021-09-23 15:20:24 +0300

    finished project 4

---
+++ 4 Board Source/app.js
@@ -0,0 +1,29 @@
+const board = document.querySelector('#board')
+const colors = ['#17c2af', '#bbc217', '#e71213', '#12e77d', '#1224c4', '#c412ba', '#45068c']
+const SQUARES_NUMBER = 500
+
+for (let i = 0; i < SQUARES_NUMBER; i++) {
+  const square = document.createElement('div')
+  square.classList.add('square')
+  board.append(square)
+
+  square.addEventListener('mouseover', () => setColor(square))
+  square.addEventListener('mouseleave', () => removeColor(square))
+}
+
+
+function setColor(element){
+  const color = getRandomColor()
+  element.style.backgroundColor = color
+  element.style.boxShadow = `0 0 2px ${color}, 0 0 10px ${color}`
+}
+
+function removeColor(element){
+  element.style.backgroundColor = '#1d1d1d';
+  element.style.boxShadow = '0 0 2px #000'
+}
+
+function getRandomColor() {
+  const colorIndex = Math.floor(Math.random() * colors.length)
+  return colors[colorIndex]
+}
