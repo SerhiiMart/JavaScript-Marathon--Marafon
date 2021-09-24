5222cac6c2c9cd2f1f6ef39dada74d5eb0a5cafb
Serhii <malgothsm@gmail.com>
2021-09-24 14:11:30 +0300

    Project #5 update 3

--- 5 AIM Game/app.js
+++ 5 AIM Game/app.js
@@ -1,1 +1,53 @@
-const
+const startBtn = document.querySelector('#start')
+const screens = document.querySelectorAll('.screen')
+const timeList = document.querySelector('#time-list')
+const timeEl = document.querySelector('#time')
+
+let time = 11
+
+startBtn.addEventListener('click', (event) =>{
+ event.preventDefault()
+ screens[0].classList.add('up')
+})
+
+timeList.addEventListener('click', (event) => {
+  if (event.target.classList.contains('time-btn')) {
+    time = parseInt(event.target.getAttribute('data-time')) // Getting the attribute and making it's type Integer
+    screens[1].classList.add('up')
+    startGame()
+  }
+})
+
+//Debug
+startGame()
+
+function startGame() {
+  setInterval(decreaseTime, 1000)
+
+  setTime(time)
+}
+
+function decreaseTime() {
+  if (time === 0) {
+    finishGame()
+  } else {
+    let current = --time
+    if (current < 10) {
+      current = `0${current}`
+    }
+    setTime(current)
+  }
+}
+
+function setTime(value) {
+  timeEl.innerHTML = `00:${value}`
+}
+
+function finishGame() {
+
+}
+
+function createRandomCircle() {
+  const circle = document.createElement('div')
+  circle.classList.add('circle')
+}
